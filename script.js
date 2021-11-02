f89889d19646291878eb18dd488d2cdab6dc52f6
hjyoon <se1620236@naver.com>
2021-11-03 07:07:22 +0900

    update

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-const addBtn = document.querySelector('#add')
-const divTag = document.querySelector('#todolist')
+const add_btn = document.querySelector('#add_btn')
+const todolist = document.querySelector('#todolist')
 
-const addTodo = (event) => {
-    event.preventDefault()
+// 리스트 삭제하기
+const removeParentNode = (event) => {
+    const target = event.target;
+    target.removeEventListener("click", removeParentNode);
+    target.parentNode.remove();
+}
 
-    const pTag = document.createElement('p')
-    const chkboxTag = document.createElement('input')
-    chkboxTag.setAttribute('type', 'checkbox')
-    pTag.appendChild(chkboxTag)
+// 체크박스 체크시 취소선 긋기
+const getCheckboxValue = (event) => {
+    //const parentNode = event.target.parentNode
+    //const nodes = parentNode.childNodes
+    // if (event.target.checked) {
+    //     nodes[1].classList.add('done')
+    // }
+    // else {
+    //     nodes[1].classList.remove('done')
+    // }
+    //nodes[1].classList.toggle('done')
+    event.target.nextSibling.classList.toggle('done')
+}
 
-    const getCheckboxValue = (event) => {
-        const parentNode = event.target.parentNode
-        const nodes = parentNode.childNodes
-        if (event.target.checked) {
-            nodes[1].classList.add('done')
-        }
-        else {
-            nodes[1].classList.remove('done')
-        }
-    }
+const makeItem = () => {
+    const item = document.createElement('div')
+    item.setAttribute('class', 'todo_item')
 
-    chkboxTag.addEventListener('click', getCheckboxValue)
+    const chkbox = document.createElement('input')
+    chkbox.setAttribute('type', 'checkbox')
+    chkbox.addEventListener('click', getCheckboxValue)
+    item.appendChild(chkbox)
 
     const inputTag = document.createElement('input')
     inputTag.setAttribute('type', 'text')
-    inputTag.setAttribute('class', 'todo_item')
-    pTag.appendChild(inputTag)
+    inputTag.setAttribute('class', 'todo_item_form')
+    item.appendChild(inputTag)
 
-    const XTag = document.createElement('button')
-    XTag.append('X')
-    pTag.appendChild(XTag)
+    const del_btn = document.createElement('button')
+    del_btn.setAttribute('class', 'del_btn')
+    del_btn.append('X')
+    item.appendChild(del_btn)
 
-    // 리스트 삭제하기
-    const removeParentNode = (event) => {
-        const target = event.target;
-        target.removeEventListener("click", removeParentNode);
-        target.parentNode.remove();
-    }
+    del_btn.addEventListener('click', removeParentNode)
 
-    XTag.addEventListener('click', removeParentNode)
-
-    divTag.appendChild(pTag)
+    return item
 }
 
-addBtn.addEventListener('click', addTodo)
+const addTodo = (event) => {
+    // event.preventDefault()
+    const item = makeItem()
+    console.log(item)
+    todolist.appendChild(item)
+}
+
+add_btn.addEventListener('click', addTodo)
